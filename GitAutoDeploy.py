dc5b2d33f8fe30e48ded2a7c45b17b96d4e30bcc
logsol <fartman@gmx.de>
2012-06-23 16:14:24 +0200

    using && instead of ; between directory change and subsequent command. fixes #2

--- GitAutoDeploy.py
+++ GitAutoDeploy.py
@@ -62,7 +62,7 @@ class GitAutoDeploy(BaseHTTPRequestHandler):
         if(not self.quiet):
             print "\nPost push request received"
             print 'Updating ' + path
-        call(['cd "' + path + '"; git pull'], shell=True)
+        call(['cd "' + path + '" && git pull'], shell=True)
 
     def deploy(self, path):
         config = self.getConfig()
@@ -71,7 +71,7 @@ class GitAutoDeploy(BaseHTTPRequestHandler):
                 if 'deploy' in repository:
                      if(not self.quiet):
                          print 'Executing deploy command'
-                     call(['cd "' + path + '";' + repository['deploy']], shell=True)
+                     call(['cd "' + path + '" && ' + repository['deploy']], shell=True)
                 break
 
 def main():
