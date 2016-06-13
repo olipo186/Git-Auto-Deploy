c4407c007ab4ac900d6438761f72acae44878054
Oliver Poignant <oliver@poignant.se>
2016-06-13 23:29:05 +0200

    Adding python-lockfile as dependency for debian package. New version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='git-auto-deploy',
-      version='0.2.8',
+      version='0.2.9',
       url='https://github.com/olipo186/Git-Auto-Deploy',
       author='Oliver Poignant',
       author_email='oliver@poignant.se',
@@ -12,6 +12,9 @@ setup(name='git-auto-deploy',
               'git-auto-deploy = gitautodeploy.__main__:main'
           ]
       },
+      install_requires=[
+            'lockfile'
+      ],
       description = "Deploy your GitHub, GitLab or Bitbucket projects automatically on Git push events or webhooks.",
       long_description = "GitAutoDeploy consists of a HTTP server that listens for Web hook requests sent from GitHub, GitLab or Bitbucket servers. This application allows you to continuously and automatically deploy you projects each time you push new commits to your repository."
 )
